285f388b547a759fb81dd23792359a3ead35cb6a
leechanghwanspace <spacecehh@naver.com>
2024-12-26 23:22:11 +0900

    feat: user entity, db set

---
+++ src/main/java/com/kdt/board/user/User.java
@@ -0,0 +1,31 @@
+package com.kdt.board.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+}
---
+++ src/main/java/com/kdt/board/user/UserRole.java
@@ -0,0 +1,5 @@
+package com.kdt.board.user;
+
+public enum UserRole {
+    USER, ADMIN
+}
